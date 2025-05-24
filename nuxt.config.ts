dff76948d55d5a5da8e165baf8e8a0e55a5e6fff
MichaelNotych <notychweb@gmail.com>
2025-05-24 18:13:00 +0700

    feat: add favicon and styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,18 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-05-15',
 	devtools: { enabled: true },
 	modules: ['@nuxt/eslint'],
+	// конфигурация для работы с head
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap',
+				},
+			],
+		},
+	},
+	css: ['~/assets/main.css'],
+
 });
