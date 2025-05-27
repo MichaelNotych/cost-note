121ea370d3166b06cad5018a7a58cc20d6ea7867
MichaelNotych <notychweb@gmail.com>
2025-05-27 22:13:08 +0700

    feat: add basic dashboard page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+definePageMeta({
+	middleware: 'auth',
+});
+
+const { user, clear } = useUserSession();
+
+const logout = async () => {
+	await clear();
+	navigateTo('/auth/signin');
+};
+</script>
+
+<template>
+	<div class="cn_dashboard">
+		<h1>Welcome to Dashboard, <span v-if="user">{{ user.name }}</span></h1>
+		<CustomButton label="Logout" variant="primary" type="button" size="small" @click="logout"/>
+	</div>
+</template>
+<style scoped>
+.cn_dashboard {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+</style>
