f60ab44dcaf6817dffe8fc830ee23a9a94c42335
MichaelNotych <notychweb@gmail.com>
2025-05-29 11:03:13 +0700

    feat: add button loading state

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -33,18 +33,18 @@ const props = defineProps({
 
 const emit = defineEmits(['click']);
 
-const extraClasses = [`cn_button_${props.variant}`, `cn_button_${props.size}`];
+const extraClasses = [`cn-button_${props.variant}`, `cn-button_${props.size}`];
 
 
 </script>
 <template>
-	<button class="cn_button" :class="extraClasses.join(' ')" :disabled="props.disabled || props.isLoading" :type="props.type" @click="emit('click')">
+	<button class="cn-button" :class="[extraClasses.join(' '), { 'cn-button_loading': props.isLoading }]" :disabled="props.disabled" :type="props.type" @click="emit('click')">
 		<component :is="props.icon" v-if="props.icon" />
 		{{ props.label }}
 	</button>
 </template>
 <style scoped>
-.cn_button {
+.cn-button {
 	background-color: var(--accent-color);
 	color: var(--accent-text-color);
 	border: none;
@@ -58,32 +58,81 @@ const extraClasses = [`cn_button_${props.variant}`, `cn_button_${props.size}`];
 	transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
 }
 
-.cn_button:hover {
+.cn-button:hover {
 	background-color: var(--accent-hover-color);
 }
 
-.cn_button_secondary {
+.cn-button_secondary {
 	background-color: var(--primary-background);
 	color: var(--text-color);
 	border: 1px solid var(--stroke-color);
 }
 
-.cn_button_secondary:hover {
+.cn-button_secondary:hover {
 	background-color: var(--secondary-background);
 }
 
-.cn_button_outline {
+.cn-button_outline {
 	background-color: var(--primary-background);
 	color: var(--accent-color);
 	border: 1px solid var(--accent-color);
 }
 
-.cn_button_outline:hover {
+.cn-button_outline:hover {
 	color: var(--accent-text-color);
 }
 
-.cn_button_small {
+.cn-button_small {
 	padding: 0.5em 1em;
 	font-size: 1rem;
+}
+
+.cn-button[disabled] {
+	opacity: 0.5;
+	pointer-events: none;
+}
+
+.cn-button_loading {
+	position: relative;
+	pointer-events: none;
+}
+
+.cn-button_loading::before {
+	content: '';
+	position: absolute;
+	inset: 0;
+	background-color: inherit;
+	border-radius: inherit;
+	z-index: 2;
+}
+
+.cn-button_loading::after {
+	content: '';
+	position: absolute;
+	left: calc(50% - 8px);
+	top: calc(50% - 8px);
+	width: 16px;
+	height: 16px;
+	border-radius: 100%;
+	z-index: 3;
+	border: 2px solid var(--accent-text-color);
+	border-top-color: transparent;
+	animation: spin 1s linear infinite;
+}
+
+.cn-button_secondary.cn-button_loading::after {
+	border-color: var(--text-color);
+	border-top-color: transparent;
+}
+
+.cn-button_outline.cn-button_loading::after {
+	border-color: var(--accent-color);
+	border-top-color: transparent;
+}
+
+@keyframes spin {
+	to {
+		transform: rotate(360deg);
+	}
 }
 </style>
