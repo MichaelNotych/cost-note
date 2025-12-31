0a7f8e4d485e247bde2ff4785f83a836b60dcfb6
MichaelNotych <notychweb@gmail.com>
2025-12-31 11:34:38 +0700

    feat: add button loader

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -1,29 +1,39 @@
 <script setup lang="ts">
-import type { PrimitiveProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import type { ButtonVariants } from "."
-import { Primitive } from "reka-ui"
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "."
+import type { PrimitiveProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+import type { ButtonVariants } from ".";
+import { Primitive } from "reka-ui";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from ".";
+import { Spinner } from "../spinner";
 
 interface Props extends PrimitiveProps {
-  variant?: ButtonVariants["variant"]
-  size?: ButtonVariants["size"]
-  class?: HTMLAttributes["class"]
+    variant?: ButtonVariants["variant"];
+    size?: ButtonVariants["size"];
+    class?: HTMLAttributes["class"];
+    loading?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  as: "button",
-})
+    as: "button",
+});
 </script>
 
 <template>
-  <Primitive
-    data-slot="button"
-    :as="as"
-    :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
-  >
-    <slot />
-  </Primitive>
+    <Primitive
+        data-slot="button"
+        :as="as"
+        :as-child="asChild"
+        :class="cn(buttonVariants({ variant, size }), props.class)"
+    >
+        <div
+            v-if="loading"
+            class="absolute inset-0 bg-inherit rounded-inherit"
+        ></div>
+        <Spinner
+            v-if="loading"
+            class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+        <slot />
+    </Primitive>
 </template>
